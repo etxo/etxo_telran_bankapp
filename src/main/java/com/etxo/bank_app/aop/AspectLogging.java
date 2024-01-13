05e07c9ff3ccfd10d17a3994dea5c9a2d7a18890
etxo <etxo@gmx.de>
2024-01-13 13:28:30 +0100

    feat: log_service

--- src/main/java/com/etxo/bank_app/aop/AspectLogging.java
+++ src/main/java/com/etxo/bank_app/aop/AspectLogging.java
@@ -1,10 +1,12 @@
 package com.etxo.bank_app.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -14,5 +16,26 @@ public class AspectLogging {
 
     @Pointcut("execution(* com.etxo.bank_app.mapping..*.*(..))")
     public void getAnyFromMapping(){}
+    @Before("getAnyFromMapping()")
+    public void logAnyCallsOfMapping(JoinPoint jp){
+        LOGGER.info("Mapper {} was invoked with parameters {}",
+                jp.getSignature().getName(), Arrays.stream(jp.getArgs()).toList());
+    }
 
+    @Pointcut("execution(* com.etxo.bank_app.service..*.*(..))")
+    public void getAnyFromServices(){}
+
+    @Before("getAnyFromServices()")
+    public void logAnyCallOfService(JoinPoint joinPoint) {
+        LOGGER.info("Method {} was invoked with parameter {}.",
+                joinPoint.getSignature(),
+                joinPoint.getArgs());
+    }
+
+    @AfterThrowing("getAnyFromServices()")
+    public void logAfterThrowingException(JoinPoint joinPoint){
+        LOGGER.info("Method {} of {} threw an exception",
+                joinPoint.getSignature(),
+                joinPoint.getSignature().getDeclaringType());
+    }
 }
