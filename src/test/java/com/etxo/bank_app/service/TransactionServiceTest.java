0043276da5127577a072a013dc74f1f35d83c053
etxo <etxo@gmx.de>
2024-01-18 07:37:48 +0100

    feat: tried_transaction_test

--- src/test/java/com/etxo/bank_app/service/TransactionServiceTest.java
+++ src/test/java/com/etxo/bank_app/service/TransactionServiceTest.java
@@ -1,7 +1,6 @@
 package com.etxo.bank_app.service;
 
 import com.etxo.bank_app.dto.AccountDto;
-import com.etxo.bank_app.dto.AccountDtoShort;
 import com.etxo.bank_app.dto.TransactionDto;
 import com.etxo.bank_app.entity.Account;
 import com.etxo.bank_app.entity.Client;
@@ -15,13 +14,12 @@ import com.etxo.bank_app.reposi.TransactionRepository;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
@@ -32,73 +30,78 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceTest {
 
     @InjectMocks
-    private TransactionService transactionService;
+    private TransactionService service;
     @Mock
-    private TransactionRepository transactionRepository;
+    private TransactionRepository transactionRepo;
     @Mock
-    private AccountRepository accountRepository;
+    private AccountRepository accountRepo;
     @Mock
     private TransactionMapping mapper;
 
-    private Account expectedSender;
-    private Account expectedReceiver;
+    private Account givenSender;
+    private Account givenReceiver;
     private Transaction expectedTransaction;
     private TransactionDto expectedTransactionDto;
+    @Captor
+    private ArgumentCaptor<Account> senderCaptor;
+    @Captor
+    private ArgumentCaptor<Account> receiverCaptor;
 
     @BeforeEach
     void init() {
 
         Faker faker = new Faker(new Locale("DE"));
 
-        expectedSender = new Account();
-        expectedSender.setId(1L);
-        expectedSender.setClient(new Client());
-        expectedSender.setIban(faker.finance().iban());
-        expectedSender.setBic(faker.finance().bic());
-        expectedSender.setAccountType(AccountType.DEBIT);
-        expectedSender.setStatus(Status.ACTIVE);
-        expectedSender.setBalance(new BigDecimal(100));
-        expectedSender.setCurrencyCode(Currency.EUR);
+        givenSender = new Account();
+        givenSender.setId(1L);
+        givenSender.setClient(new Client());
+        givenSender.setIban(faker.finance().iban());
+        givenSender.setBic(faker.finance().bic());
+        givenSender.setAccountType(AccountType.DEBIT);
+        givenSender.setStatus(Status.ACTIVE);
+        givenSender.setBalance(new BigDecimal(100));
+        givenSender.setCurrencyCode(Currency.EUR);
 
         AccountDto expectedSenderDto = new AccountDto();
         expectedSenderDto.setId(1L);
-        expectedSenderDto.setIban(expectedSender.getIban());
-        expectedSenderDto.setBic(expectedSender.getBic());
-        expectedSenderDto.setBalance(expectedSender.getBalance());
+        expectedSenderDto.setIban(givenSender.getIban());
+        expectedSenderDto.setBic(givenSender.getBic());
+        expectedSenderDto.setBalance(givenSender.getBalance());
 
-        expectedReceiver = new Account();
-        expectedReceiver.setId(2L);
-        expectedReceiver.setClient(new Client());
-        expectedReceiver.setIban(faker.finance().iban());
-        expectedReceiver.setBic(faker.finance().bic());
-        expectedReceiver.setAccountType(AccountType.DEBIT);
-        expectedReceiver.setStatus(Status.ACTIVE);
-        expectedReceiver.setBalance(new BigDecimal(100));
-        expectedReceiver.setCurrencyCode(Currency.EUR);
+        givenReceiver = new Account();
+        givenReceiver.setId(2L);
+        givenReceiver.setClient(new Client());
+        givenReceiver.setIban(faker.finance().iban());
+        givenReceiver.setBic(faker.finance().bic());
+        givenReceiver.setAccountType(AccountType.DEBIT);
+        givenReceiver.setStatus(Status.ACTIVE);
+        givenReceiver.setBalance(new BigDecimal(100));
+        givenReceiver.setCurrencyCode(Currency.EUR);
 
-        AccountDto expectedReceiverDto = new AccountDto();
-        expectedReceiverDto.setId(2L);
-        expectedReceiverDto.setIban(expectedReceiver.getIban());
-        expectedReceiverDto.setBic(expectedReceiver.getBic());
-        expectedReceiverDto.setBalance(expectedReceiver.getBalance());
+        AccountDto givenReceiverDto = new AccountDto();
+        givenReceiverDto.setId(2L);
+        givenReceiverDto.setIban(givenReceiver.getIban());
+        givenReceiverDto.setBic(givenReceiver.getBic());
+        givenReceiverDto.setBalance(givenReceiver.getBalance());
 
         expectedTransaction = new Transaction();
         expectedTransaction.setId(1L);
-        expectedTransaction.setSender(expectedSender);
-        expectedTransaction.setReceiver(expectedReceiver);
-        expectedTransaction.setAmount(new BigDecimal(50));
+        expectedTransaction.setSender(givenSender);
+        expectedTransaction.setReceiver(givenReceiver);
+        expectedTransaction.setAmount(new BigDecimal(25));
 
         expectedTransactionDto = new TransactionDto();
         expectedTransactionDto.setId(1L);
         expectedTransactionDto.setSender(expectedSenderDto);
-        expectedTransactionDto.setReceiver(expectedReceiverDto);
-        expectedTransactionDto.setAmount(new BigDecimal(50));
+        expectedTransactionDto.setReceiver(givenReceiverDto);
+        expectedTransactionDto.setAmount(new BigDecimal(25));
 
     }
 
@@ -110,26 +113,34 @@ class TransactionServiceTest {
     //@Disabled
     void itShouldExecuteTransaction() {
 
-        when(accountRepository.existsById(anyLong())).thenReturn(true);
-        when(accountRepository.findById(1L)).thenReturn(Optional.of(expectedSender));
-        when(accountRepository.findById(2L)).thenReturn(Optional.of(expectedReceiver));
-        when(accountRepository.save(any(Account.class))).thenReturn(expectedSender);
+        when(accountRepo.existsById(anyLong())).thenReturn(true);
+        when(accountRepo.findById(1L)).thenReturn(Optional.of(givenSender));
+        when(accountRepo.findById(2L)).thenReturn(Optional.of(givenReceiver));
+
+        when(accountRepo.save(any(Account.class))).thenReturn(givenSender);
         when(mapper.mapToEntity(expectedTransactionDto)).thenReturn(expectedTransaction);
-        when(transactionRepository.save(any(Transaction.class))).thenReturn(expectedTransaction);
+        when(transactionRepo.save(any(Transaction.class))).thenReturn(expectedTransaction);
         when(mapper.mapToDto(expectedTransaction)).thenReturn(expectedTransactionDto);
 
-        TransactionDto savedDto = transactionService.execute(expectedTransactionDto);
+        TransactionDto savedDto = service.execute(expectedTransactionDto);
+        //verify(accountRepo).save(senderCaptor.capture());
+        //Account savedSender = senderCaptor.getValue();
+        //verify(accountRepo).save(receiverCaptor.capture());
+        //Account savedReceiver = receiverCaptor.getValue();
+
         assertEquals(expectedTransactionDto, savedDto);
+        //assertEquals(new BigDecimal(75), savedSender.getBalance());
+        //assertEquals(new BigDecimal(125), savedReceiver.getBalance());
     }
 
     @Test
     void itShouldGetTransactionsByClientId() {
 
-        when(transactionRepository.findTransactionsByClientId(anyLong()))
+        when(transactionRepo.findTransactionsByClientId(anyLong()))
                 .thenReturn(List.of(expectedTransaction));
         when(mapper.mapToDto(any(Transaction.class))).thenReturn(expectedTransactionDto);
 
         assertIterableEquals(List.of(expectedTransactionDto),
-                transactionService.getTransactionsByClientId(1L));
+                service.getTransactionsByClientId(1L));
     }
 }
