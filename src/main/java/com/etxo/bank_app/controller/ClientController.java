9e2a786f084b70203d82295a05620248bc2fdb76
etxo <etxo@gmx.de>
2024-01-24 21:26:08 +0100

    fix: swagger_client

--- src/main/java/com/etxo/bank_app/controller/ClientController.java
+++ src/main/java/com/etxo/bank_app/controller/ClientController.java
@@ -4,6 +4,7 @@ import com.etxo.bank_app.dto.ClientDto;
 import com.etxo.bank_app.dto.ClientDtoUpdate;
 import com.etxo.bank_app.security.service.UserService;
 import com.etxo.bank_app.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,8 @@ public class ClientController {
     private final ClientService service;
     private final UserService userService;
 
+
+    @Operation(summary = "gets all clients")
     @Secured({"ADMIN", "MANAGER"})
     @GetMapping
     public ResponseEntity<Set<ClientDto>> getClients(){
@@ -28,6 +31,7 @@ public class ClientController {
         return ResponseEntity.ok(service.getClients());
     }
 
+    @Operation(summary = "gets client by id")
     @ApiResponse(responseCode = "404",
                     description = "no client with this id")
     @GetMapping("/{id}")
@@ -37,6 +41,7 @@ public class ClientController {
         return ResponseEntity.ok(service.getClientById(id));
     }
 
+    @Operation(summary = "gets client by email")
     @ApiResponse(responseCode = "404",
                     description = "no client with this email")
     @GetMapping("/by_email/{email}")
@@ -52,6 +57,8 @@ public class ClientController {
         else return ResponseEntity.badRequest().body(null);
     }
 
+    @Operation(summary = "creates client",
+                description = "client must not exist in the database")
     @ApiResponse(responseCode = "404",
                     description = "email exists already")
     @PostMapping
@@ -62,6 +69,10 @@ public class ClientController {
         return ResponseEntity.ok(service.create(client));
     }
 
+    @Operation(summary = "updates client by id",
+            description = "client should exist in the database")
+    @ApiResponse(responseCode = "404",
+            description = "client not found")
     @PutMapping("/update/{id}")
     @Secured("MANAGER")
     public ResponseEntity<ClientDto> update(
@@ -70,6 +81,10 @@ public class ClientController {
         return ResponseEntity.ok(service.updateById(id, dto));
     }
 
+    @Operation(summary = "deactivates client by id",
+            description = "client should exist in the database")
+    @ApiResponse(responseCode = "404",
+            description = "client not found")
     @DeleteMapping("/{id}")
     @Secured("MANAGER")
     public ResponseEntity<ClientDto> delete(@PathVariable Long id){
